290e774047a29813ed5508bed6ba44f766824431
RiskiGanz <98639032+RiskiRj@users.noreply.github.com>
2022-04-24 11:34:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var audio = new Audio('assets/sentmessage.mp3');
-var contactString = "<div class='social'> <a target='_blank' href='tel:+918600765857'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>89501060783</label></div> </a> <a href='daffayudhistira93@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/DEFF1602'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/6289501060783'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> </a> <a target='_blank' href='https://t.me/Daffa'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/deff.xyz'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/DEFF' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
-var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Daffa Resume.pdf</label></div><a href='assets/Resume.pdf' download='DEFF_Resume.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
-var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>'Jakarta'<br>JakBar<br>Kebon Jeruk, KebonJeruk INDONESIA 11530</address>";
+var contactString = "<div class='social'> <a target='_blank' href='tel:+6281339888334'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>6281339888334</label></div> </a> <a href='RiskiGx22@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/RiskiRj'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/6281339888334'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> </a> <a target='_blank' href='https://t.me/Riski'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/Riski Ganz'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/Riski' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Riski Resume.pdf</label></div><a href='assets/Resume.pdf' download='Riski_Resume.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
+var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>'NTT'<br>Flores<br>Ende, Ende INDONESIA 11530</address>";
 function startFunction() {
     setLastSeen();
     waitAndResponce("intro");
@@ -72,7 +72,7 @@ function waitAndResponce(inputText) {
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Daffa Yudhistira</a>.</span><br><br>I am a Newbie student at <span class='bold'>Ch. Smpn 88 👨🏻‍💻📚</span><br><br>Web Ini Dibuat Untuk Iseng² Saja.<br><br>Send <span class='bold'>'help'</span> -_-.<br>");
+                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>RiskiGanz</a>.</span><br><br>I am a Newbie student at <span class='bold'>Ch. MTS 👨🏻‍💻📚</span><br><br>Web Ini Dibuat Untuk Iseng² Saja.<br><br>Send <span class='bold'>'help'</span> -_-.<br>");
             }, 2000);
             break;
         case "help":
@@ -82,11 +82,11 @@ function waitAndResponce(inputText) {
             sendTextMessage(resumeString);
             break;
         case "skills":
-            sendTextMessage("<span class='sk'>I am currently study in Smpn 88<br><br>Cuma Jago Jadi Copaster Dan mencoba belajar bahasa berikut:)<br><span class='bold'>Java<br>C++<br>C<br>PHP<br>Kotlin<br>Dart<br>Python<br>CSS<br>HTML</span><br><br>I've experiance with following frameworks 😜😜:<span class='bold'><br>Android<br>Flutter<br>ReactJs<br>GTK</span><br><br>I use <span class='bold'>Termux</span> as daily driver on my Redmi Note 9<br>OS: Android<br>DE:Gnome(More often) Kde(often)<br>Favourite IDE:MT MANAGER</span>");
+            sendTextMessage("<span class='sk'>I am currently study in MTS<br><br>Cuma Jago Jadi Copaster Dan mencoba belajar bahasa berikut:)<br><span class='bold'>Java<br>C++<br>C<br>PHP<br>Kotlin<br>Dart<br>Python<br>CSS<br>HTML</span><br><br>I've experiance with following frameworks 😜😜:<span class='bold'><br>Android<br>Flutter<br>ReactJs<br>GTK</span><br><br>I use <span class='bold'>Termux</span> as daily driver on my Redmi Note 9<br>OS: Android<br>DE:Gnome(More often) Kde(often)<br>Favourite IDE:MT MANAGER</span>");
             break;
 
         case "education":
-            sendTextMessage("I am currently study in Smpn 88<br>Bentar lagi lulus si hehe <br><br>Sebelum di 88 gw sekolah di sdn 17<br>Passing Year: 2020 <br>Result:86.06%<br><br>My Bithday is :v<br>16 February 2007 ");
+            sendTextMessage("I am currently study in Smpn MTS<br>Bentar lagi mau naik kelas 9 si hehe <br><br>Sebelum di MTS gw sekolah di Min 1 Ende<br>Passing Year: 2020 <br>Result:86.06%<br><br>My Bithday is :v<br>02 Agustus 2007 ");
             break;
 
         case "address":
@@ -96,13 +96,13 @@ function waitAndResponce(inputText) {
             clearChat();
             break;
         case "about":
-            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https:\/\/instagram.com/deff.xyz/'><span class='bold'>Daffa</a> with ❤️</span>");
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https:\/\/instagram.com/RiskiGanz/'><span class='bold'>RiskiGanz</a> with ❤️</span>");
             break;
         case "contact":
             sendTextMessage(contactString);
             break;
         case "projects":
-            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/DEFF1602'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/RiskiRj'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
             break;
         case "new":
             sendTextMessage(addressString);
